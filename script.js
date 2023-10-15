d13b61d179f787c804ff81817adb2bc0389fa505
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2023-10-15 14:09:52 -0700

    Actually make it based on browsers preferred theme

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleDarkmode() {
    document.body.classList.toggle("light-mode");
    document.body.classList.toggle("dark-mode");
 }
+
+if (window.matchMedia) {
+   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.toggle("dark-mode");
+   }
+} else {
+   document.body.classList.toggle("light-mode");
+}
