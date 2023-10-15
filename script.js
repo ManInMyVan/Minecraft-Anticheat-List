37955f3309a3953b739c0fad67c9b39b7088f7e7
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2023-10-15 14:22:22 -0700

    Remove unnecessary lines

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ function toggleDarkmode() {
 }
 
 function load() {
-   if (window.matchMedia) {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-         document.body.classList.toggle("dark-mode");
-      }
+   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.toggle("dark-mode");
    } else {
       document.body.classList.toggle("light-mode");
    }
