ac333dd50e7d65bbbe608f6c24d2da707098f743
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2023-10-15 14:10:58 -0700

    .

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function toggleDarkmode() {
    document.body.classList.toggle("dark-mode");
 }
 
-if (window.matchMedia) {
-   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.classList.toggle("dark-mode");
+function load() {
+   if (window.matchMedia) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+         document.body.classList.toggle("dark-mode");
+      }
+   } else {
+      document.body.classList.toggle("light-mode");
    }
-} else {
-   document.body.classList.toggle("light-mode");
 }
