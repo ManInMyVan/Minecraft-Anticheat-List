320a7bc41df58d426de3d898ceb3f4db7569d9e9
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2023-10-15 19:38:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function toggleLightmode() {
-   document.body.classList.toggle("light-mode");
-}
-function toggleDarkmode() {
-   document.body.classList.toggle("dark-mode");
-}
 function toggleTheme() {
-   toggleDarkmode()
-   toggleLightmode()
-}
-function load() {
-   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      toggleDarkmode()
-   } else {
-      toggleLightmode()
-   }
+   document.body.classList.toggle("dark-mode");
+   document.body.classList.toggle("light-mode");
 }
