2f0e93067511b927ebe4810666a6ecabbc26688e
ManInMyVan <maninmyvan@gmail.com>
2023-12-27 18:52:37 -0700

    fix

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ function toggleTheme() {
    document.body.classList.toggle("dark-mode")
    document.body.classList.toggle("light-mode")
 }
-
-updateRating(83198) // AntiHaxerman
-updateRating(111260) // FoxAddition
-updateRating(112706) // Gato Anticheat
-updateRating(111084) // Ghost
-updateRating(99923) // Grim
-updateRating(112053) // LiteAntiCheat
-updateRating(113149) // Spidey Anticheat v2
-updateRating(90766) // Themis
-updateRating(48399) // Negativity v1
-updateRating(113143) // Spidey Anticheat v1
+async function updateRatings() {
+   updateRating(83198) // AntiHaxerman
+   updateRating(111260) // FoxAddition
+   updateRating(112706) // Gato Anticheat
+   updateRating(111084) // Ghost
+   updateRating(99923) // Grim
+   updateRating(112053) // LiteAntiCheat
+   updateRating(113149) // Spidey Anticheat v2
+   updateRating(90766) // Themis
+   updateRating(48399) // Negativity v1
+   updateRating(113143) // Spidey Anticheat v1
+}
 
 async function fetchData(resourceid) {
    var data
@@ -25,6 +26,7 @@ async function fetchData(resourceid) {
       })
    return data
 }
+
 async function updateRating(resourceid) {
    var data = await fetchData(resourceid)
    document.getElementById(resourceid+"-rating").innerHTML = '<b>'+(data.rating.average * 20).toFixed(0)+'%</b>, '+data.rating.count+' rating'
