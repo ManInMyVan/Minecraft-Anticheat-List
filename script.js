11f7e21e6aa0883a221e6443f4b6db56627eb900
ManInMyVan <maninmyvan@gmail.com>
2024-01-12 11:44:32 -0700

    move the anticheats list in the web version to a json file

--- script.js
+++ script.js
@@ -1,397 +1,49 @@
-// TODO: move to a dif file
-const anticheats = [
-   /*
-    * resourceid:
-    *    if set, the rating will be fetched from spiget (spigotmc api),
-    *    must be on spigotmc.
-    *    set it to null for it to not do so.
-    */
-   {
-      name: "Grim",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8, 1.12, 1.16 - 1.20.2",
-      resourceid: 99923,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/99923"
-      }, {
-         name: "GitHub",
-         url: "https://github.com/GrimAnticheat/Grim"
-      }, {
-         name: "Hanger",
-         url: "https://hangar.papermc.io/GrimAnticheat/GrimAnticheat"
-      }, {
-         name: "Dedicated Site",
-         url: "https://grim.ac"
-      }]
-   }, {
-      name: "AntiAura v2",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8 - 1.19",
-      resourceid: 1368,
-      rating: null,
-      price: "£12.00 (GBP)", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/1368"
-      }]
-   }, {
-      name: "Ghost",
-      plaform: "Bukkit (Java)",
-      status: "Discontinued",
-      versions: "1.7 - 1.20",
-      resourceid: 111084,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/111084"
-      }]
-   }, {
-      name: "AntiHaxerman",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.7 - 1.8",
-      resourceid: 83198,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/83198"
-      },{
-         name: "GitHub",
-         url: "https://github.com/Tecnio/AntiHaxerman"
-      }]
-   }, {
-      name: "Cardinal",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.8 - 1.20",
-      resourceid: null,
-      rating: "Unknown",
-      price: "Free, €19.99 (EUR)", 
-      links: [{
-         name: "Dedicated Site",
-         url: "https://cardinalanticheat.github.io/documentation"
-      },{
-         name: "GitHub",
-         url: "https://github.com/micartey/Cardinal-Anticheat"
-      }, {
-         name: "BuiltByBit",
-         url: "https://builtbybit.com/resources/26622"
-      }]
-   }, {
-      name: "FoxAddition",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.7.2 - 1.20.4",
-      resourceid: 111260,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/111260"
-      }]
-   }, {
-      name: "Updated NoCheatPlus",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.5 - 1.20",
-      resourceid: null,
-      rating: "Unknown",
-      price: "Free", 
-      links: [{
-         name: "GitHub",
-         url: "https://github.com/Updated-NoCheatPlus/NoCheatPlus"
-      }]
-   }, {
-      name: "Themis",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.17 - 1.20.1",
-      resourceid: 90766,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/90766"
-      }]
-   }, {
-      name: "Gato Anticheat",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8 - 1.20.2",
-      resourceid: 112706,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/112706"
-      }]
-   }, {
-      name: "LiteAntiCheat",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.13 - 1.20",
-      resourceid: 112053,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/112053"
-      }, {
-         name: "GitHub",
-         url: "https://github.com/tiredvekster/LiteAntiCheat"
-      }]
-   }, {
-      name: "Spidey Anticheat v2",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8",
-      resourceid: 113149,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/113149"
-      }]
-   }, {
-      name: "AntiCheat (by PixelCraft55)",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.16 - 1.19",
-      resourceid: 109375,
-      rating: null,
-      price: "Free", 
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/109375"
-      }]
-   }, {
-      name: "Negativity v1",
-      plaform: "Bukkit (Java)",
-      status: "Discontinued",
-      versions: "1.8 - 1.20.1",
-      resourceid: 48399,
-      rating: null,
-      price: "Free",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/48399"
-      }, {
-         name: "GitHub",
-         url: "https://github.com/Elikill58/Negativity"
-      }]
-   }, {
-      name: "Spidey Anticheat v1",
-      plaform: "Bukkit (Java)",
-      status: "Discontinued",
-      versions: "1.8",
-      resourceid: 113143,
-      rating: null,
-      price: "Free",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/48399"
-      }]
-   }, {
-      name: "AntiCheatAddition",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8, 1.12, 1.15 - 1.20.2",
-      resourceid: 33590,
-      rating: null,
-      price: "€14.99 (EUR)",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/33590"
-      }]
-   }, {
-      name: "Astro",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.8+",
-      resourceid: null,
-      rating: "Unknown",
-      price: "$30.00 (?)",
-      links: [{
-         name: "Dedicated Site",
-         url: "https://astroac.cc"
-      }, {
-         name: "Discord",
-         url: "https://discord.gg/fJGspEJnyh"
-      }]
-   }, {
-      name: "Better Bubble",
-      plaform: "Skript (Java)",
-      status: null,
-      versions: "1.13 - 1.19.4",
-      resourceid: 81404,
-      rating: null,
-      price: "Free",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/81404"
-      }]
-   }, {
-      name: "GodsEye",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8 - 1.20",
-      resourceid: 69595,
-      rating: null,
-      price: "£15.50 (GBP)",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/69595"
-      }]
-   }, {
-      name: "Matrix",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.8.8, 1.12.2 - 1.20.4",
-      resourceid: 69595,
-      rating: "≈100%, 53 ratings",
-      price: "$22.00, $129.00, $1300.00 (?)",
-      links: [{
-         name: "BuiltByBit",
-         url: "https://builtbybit.com/resources/13999"
-      }, {
-         name: "Dedicated Site",
-         url: "https://matrix.rip"
-      }, {
-         name: null,
-         url: "https://spigotmc.org/resources/64635"
-      }]
-   }, {
-      name: "Intave",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.8 - 1.20",
-      resourceid: null,
-      rating: "Unknown",
-      price: "€99.99 (EUR)",
-      links: [{
-         name: "Dedicated Site",
-         url: "https://intave.ac"
-      }]
-   }, {
-      name: "Verus",
-      plaform: "Bukkit (Java)",
-      status: "Active",
-      versions: "1.7 - 1.20.1",
-      resourceid: null,
-      rating: "Unknown",
-      price: "$60.00, $125.00, $200.00 (?)",
-      links: [{
-         name: "Dedicated Site",
-         url: "https://verus.ac"
-      }, {
-         name: "Discord",
-         url: "https://discord.gg/G9RGfuZ"
-      }]
-   }, {
-      name: "Vulcan",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.7 - 1.20.4",
-      resourceid: 83626,
-      rating: null,
-      price: "$19.99 (USD)",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/83626"
-      }, {
-         name: "Discord",
-         url: "https://discord.com/invite/SCNuwUG"
-      }]
-   }, {
-      name: "Wraith",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8.8 - 1.20.1",
-      resourceid: 66887,
-      rating: null,
-      price: "€4.99 (EUR)",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/66887"
-      }]
-   }, {
-      name: "AAC",
-      plaform: "Bukkit (Java)",
-      status: null,
-      versions: "1.8 - 1.16",
-      resourceid: 6442,
-      rating: null,
-      price: "£20.00 (GBP)",
-      links: [{
-         name: "SpigotMC",
-         url: "https://spigotmc.org/resources/6442"
-      }, {
-         name: "Discord",
-         url: "https://discord.com/invite/uc9w2nx"
-      }]
-   }, {
-      name: "zHack",
-      plaform: "Bukkit (Java)",
-      status: "Unknown",
-      versions: "1.8.8",
-      resourceid: null,
-      rating: "73%, 3 ratings",
-      price: "Free",
-      links: [{
-         name: "Polymart",
-         url: "https://polymart.org/resource/460"
-      }]
-   },
-]
+load()
 
-var index = 0
-const numberofanticheats = 163
-document.getElementById("number-of-anticheats").innerHTML = `Number of anticheats: ${numberofanticheats} (${anticheats.length} included)`
-anticheats.forEach(async element => {
+async function load() {
+   var anticheats = await (await fetch("./anticheats.json")).json()
+   var index = 0
+   const numberofanticheats = 163
 
-   if (element.resourceid != null && (element.status == null || element.rating == null)) {
-      data = await fetchData(element.resourceid)
-      if (element.status == null) element.status = `${10_368_000_000 > Date.now()-(data.updateDate*1000) ? 'Active' : 'Old'}`
-      if (element.rating == null) element.rating = `<b>${(data.rating.average * 20).toFixed(0)}%</b>, ${data.rating.count} rating${data.rating.count == 1 ? '' : 's'}`
-   }
+   document.getElementById("number-of-anticheats").innerHTML = `Number of anticheats: ${numberofanticheats} (${anticheats.length} included)`
+   anticheats.forEach(async element => {
 
-   ++index
+      if (element.resourceid != null && (element.status == null || element.rating == null)) {
+         data = await fetchData(element.resourceid)
+         if (element.status == null) element.status = `${10_368_000_000 > Date.now()-(data.updateDate*1000) ? 'Active' : 'Old'}`
+         if (element.rating == null) element.rating = `${(data.rating.average * 20).toFixed(0)}%, ${data.rating.count} rating${data.rating.count == 1 ? '' : 's'}`
+      }
 
-   // couldn't think of a better way to do this 
-   if (index >= anticheats.length) { anticheats
-      .sort(function (a, b) { 
-         if (a.status == "Active" && b.status != "Active") return -1
-         else if (a.status != "Active" && b.status == "Active") return 1
-         else return a.name.localeCompare(b.name)
-      })
+      ++index
 
-      .forEach(element => {
-         var links = []
-         element.links
-            .sort(function (a,b) {return -a.name.localeCompare(b.name)})
-            .forEach(element => {if (element.name != null) links.push(`<a href="${element.url}">${element.name}</a>`)})
-         document.getElementById("anticheat-table").innerHTML +=
-            `<tr>
-               <td>${element.name}</td>
-               <td>${element.plaform}</td>
-               <td>${element.status}</td>
-               <td>${element.versions}</td>
-               <td>${element.rating}</td>
-               <td>${element.price}</td>
-               <td>${links.join(", ")}</td>
-            </tr>
-            `
-      })
-   }
-})
+      // couldn't think of a better way to do this 
+      if (index >= anticheats.length) { anticheats
+         .sort(function (a, b) { 
+            if (a.status == "Active" && b.status != "Active") return -1
+            else if (a.status != "Active" && b.status == "Active") return 1
+            else return a.name.localeCompare(b.name)
+         })
 
+         .forEach(element => {
+            var links = []
+            element.links
+               .sort(function (a,b) {return -a.name.localeCompare(b.name)})
+               .forEach(element => {if (element.name != null) links.push(`<a href="${element.url}">${element.name}</a>`)})
+            document.getElementById("anticheat-table").innerHTML +=
+               `<tr>
+                  <td>${element.name}</td>
+                  <td>${element.platform}</td>
+                  <td>${element.status}</td>
+                  <td>${element.versions}</td>
+                  <td>${element.rating}</td>
+                  <td>${element.price}</td>
+                  <td>${links.join(", ")}</td>
+               </tr>
+               `
+         })
+      }
+   })
+}
 async function toggleTheme() {
    document.body.classList.toggle("dark-mode")
    document.body.classList.toggle("light-mode")
