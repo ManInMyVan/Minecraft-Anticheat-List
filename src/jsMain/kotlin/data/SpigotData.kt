6db609fb37fac6b915aa2a23074b193fd8a40473
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2026-01-06 01:15:39 -0700

    no ratings = no data

--- src/jsMain/kotlin/data/SpigotData.kt
+++ src/jsMain/kotlin/data/SpigotData.kt
@@ -1,8 +1,8 @@
 package data
 
-class SpigotData(val rating: String, val lastUpdate: Double) {
+class SpigotData(val rating: String?, val lastUpdate: Double) {
     constructor(data: dynamic) : this(
-        "${(data.rating.average as Double * 20).toInt()}%, ${data.rating.count} ${if (data.rating.count == 1) "rating" else "ratings"}",
+        if (data.rating.count == 0) null else "${(data.rating.average as Double * 20).toInt()}%, ${data.rating.count} ${if (data.rating.count == 1) "rating" else "ratings"}",
         data.updateDate as Double * 1000
     )
 }
